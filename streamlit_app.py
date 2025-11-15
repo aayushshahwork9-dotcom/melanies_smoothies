ec2f881ad6962dea0d82287ad4b675a9be2b7da4
aayushshahwork9-dotcom <aayushshah.work9@gmail.com>
2025-11-15 21:06:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+ # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -23,7 +23,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information ')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
