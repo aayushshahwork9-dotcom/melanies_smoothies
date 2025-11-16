bb8f01b0b1e5713922fe53c7f279c8cb7328afe4
aayushshahwork9-dotcom <aayushshah.work9@gmail.com>
2025-11-17 04:16:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
